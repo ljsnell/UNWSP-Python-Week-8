d6b9f427ed76ae5571d07c2e918181effc8249bb
ljsnell <ljsnell7@gmail.com>
2024-09-10 19:35:43 -0500

    Added unit test for program 1.

--- program_1.py
+++ program_1.py
@@ -2,3 +2,18 @@
 # Write a program that gets a string containing a person's first, middle, and last names, 
 # and displays their first, middle, and last initials.  
 # For example, if the user enters John William Smith, the program should display J. W. S.
+
+# Add your logic starting on line 11
+
+def initials_generator(personsName):
+
+    personsInitials = ""
+    #    Add your logic here
+
+    return personsInitials.strip()
+
+personsName = input('Enter the users first, middle, and last name')
+
+initials = initials_generator(personsName)
+
+print(initials)
