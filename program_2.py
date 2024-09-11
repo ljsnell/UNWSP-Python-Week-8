a9b91f2d5c24a8ffad41fc01ba6542e51e8f1643
ljsnell <ljsnell@users.noreply.github.com>
2024-09-10 19:38:04 -0500

    Unit tests (#1)
    * Initial unit test
    
    * Added unit test for program 1.
    
    * Updated for clarity.

---
+++ program_2.py
@@ -0,0 +1,23 @@
+# Program #2: Word Separator
+# Write a program that accepts as input a sentence in which all of the words are run together, 
+# but the first character of each word is uppercase.  
+# Convert the sentence to a string in which the words are separated by spaces, 
+# and the first word starts with an uppercase.  
+# For example the string "StopAndSmellTheRoses" would be converted to "Stop and smell the roses."
+
+# Start your changes on line 13
+
+def word_separator(sentence):
+
+    new_sentence = ""
+    #    Add your logic here
+
+    return new_sentence.strip()
+
+# Example usage
+
+sentence = "StopAndSmellTheRoses"
+
+new_sentence = word_separator(sentence)
+
+print(new_sentence)
---
+++ test_program_1.py
@@ -0,0 +1,4 @@
+import program_1
+
+def test_word_separator():
+    assert 'J. W. S.' == program_1.initials_generator('John William Smith')
