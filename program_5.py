19af52465509372ff39f302d42da0aa44faa9c09
ljsnell <ljsnell7@gmail.com>
2024-09-10 19:37:44 -0500

    Updated for clarity.

--- program_5.py
+++ program_5.py
@@ -1,5 +1,5 @@
 # Program #5: Course Info
-# Write a program that has the user input a bunch of course ID, course name pairs.  
+# Write a program that has the user input a bunch of course ID and course name pairs.  
 # For example a course ID could be "COS 2005" and the course name could be "Python Programming."   
 # Then ask the user for a subject (like "COS"). 
-# Finally,  the program will display the ID and name of all the courses having that subject.
+# Finally, the program will display the ID and name of all the courses having that subject.
