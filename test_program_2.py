6388103a9da947dd741b033a9879673daa06036a
ljsnell <ljsnell@users.noreply.github.com>
2025-10-24 22:48:41 -0500

    Update test_program_2.py

--- test_program_2.py
+++ test_program_2.py
@@ -1,4 +1,4 @@
 import program_2
 
 def test_word_separator():
-    assert 'stop and smell the roses' == program_2.word_separator('StopAndSmellTheRoses')
+    assert 'stop and smell the roses' == program_2.word_separator('StopAndSmellTheRoses').lower()
